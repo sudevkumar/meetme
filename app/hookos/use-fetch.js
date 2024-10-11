4f22d70accbf2191b6543bfce3cb1824b73b0217
Sudev Kumar <sudevkumar82@gmail.com>
2024-10-11 18:55:12 +0530

    Updates.

--- app/hookos/use-fetch.js
+++ app/hookos/use-fetch.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 const useFetch = (cb) => {
+  // for calling the apis
   const [data, setData] = useState(undefined);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(null);
